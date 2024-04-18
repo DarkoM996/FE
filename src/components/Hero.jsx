be81173f0fa9d7353b92272c42265b5334da3e74
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-04-18 23:17:17 +0200

    latest

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import hero from "../images/hero.jpg";
 const Hero = () => {
   return (
     <div className="max-w-screen-3xl mx-auto w-full h-full">
-      <img src={hero} alt="heroimage" className="w-full h-full" />
+      <img src={hero} alt="heroimage" className="object-cover object-center" />
     </div>
   );
 };
