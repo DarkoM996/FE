0a0ae6a11c55f33768e3bfbc3a01ff27e22d5d89
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-04-20 13:27:36 +0200

    Final fix of hero section for now

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,9 +4,9 @@ import hero from "../images/hero.jpg";
 const Hero = () => {
   return (
     // this h-class can be changed later on for resposiveness
-    <div className="h-screen max-w-screen-3xl mx-auto space-y-4  bg-stone-50">
+    <div className="h-2/3 max-w-screen-3xl mx-auto space-y-4  bg-stone-50">
       {/* Image Container */}
-      <div className="h-[65%] min-w-min px-6 py-2 lg:px-8 lg:py-4 relative">
+      <div className="h-[calc(100vh-18rem)] min-w-min px-6 py-2 lg:px-8 lg:py-4 relative">
         <img
           src={hero}
           alt="hero image"
