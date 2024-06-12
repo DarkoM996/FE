b075034c2bfa58a17765bc50cab376ca94578c45
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-06-12 15:46:57 +0200

    latest

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,5 @@
 import Hero from "../components/Hero";
 import Categories from "../components/Categories";
-import FeaturedProducts from "../components/FeaturedProducts";
 import BelovedProductsByOurCustomers from "../components/BelovedProductsByOurCustomers";
 import TrendingProducts from "../components/TrendingProducts";
 import FrequentlyAskedQuestions from "../components/FrequentlyAskedQuestions";
