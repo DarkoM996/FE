adb9abc3e22718c448a3846acb469f0ac8831805
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-06-13 15:29:29 +0200

    Testimonial Section Done

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -6,10 +6,11 @@ const Testimonials = () => {
     <div className="max-w-screen-3xl mx-auto px-6 py-4 relative w-full h-full">
       <h2 className="text-4xl md:text-5xl py-12">Testimonials</h2>
       {/* Grid with testimonials */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 overflow-hidden">
         {/* Grid Element */}
-        <div className="bg-stone-300 px-8 py-6 rounded-3xl border border-stone-200 hover:shadow-2xl">
+        <div className="bg-stone-300 px-8 py-6 rounded-3xl border border-stone-400 hover:shadow-2xl">
           <h4 className="text-3xl pb-1">Some title</h4>
+          {/* Star rating  */}
           <div className="flex flex-row pb-4 text-xl text-yellow-500">
             <MdOutlineStarPurple500 />
             <MdOutlineStarPurple500 />
@@ -17,7 +18,7 @@ const Testimonials = () => {
             <MdOutlineStarPurple500 />
             <MdOutlineStarPurple500 />
           </div>
-          <p>
+          <p className="text-base leading-normal">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem,
             doloremque expedita neque velit ipsum perspiciatis, temporibus enim,
             nesciunt cumque corrupti tempora ad dicta animi ullam explicabo nisi
@@ -31,7 +32,131 @@ const Testimonials = () => {
             <p>24/05/2024</p>
           </div>
         </div>
-        {/* Grid Element #2 */}
+        {/* Grid Element */}
+        <div className="bg-stone-300 px-8 py-6 rounded-3xl border border-stone-400 hover:shadow-2xl">
+          <h4 className="text-3xl pb-1">Some title</h4>
+          {/* Star rating  */}
+          <div className="flex flex-row pb-4 text-xl text-yellow-500">
+            <MdOutlineStarPurple500 />
+            <MdOutlineStarPurple500 />
+            <MdOutlineStarPurple500 />
+            <MdOutlineStarPurple500 />
+            <MdOutlineStarPurple500 />
+          </div>
+          <p className="text-base leading-normal">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem,
+            doloremque expedita neque velit ipsum perspiciatis, temporibus enim,
+            nesciunt cumque corrupti tempora ad dicta animi ullam explicabo nisi
+            dolor doloribus dolore.
+          </p>
+          {/* Date and name of who posted it */}
+          <div className="flex flex-row justify-between items-centers pt-4">
+            {/* customer's name */}
+            <p className="font-bold">John Wayne</p>
+            {/* date when the comment is posted */}
+            <p>24/05/2024</p>
+          </div>
+        </div>
+        {/* Grid Element */}
+        <div className="bg-stone-300 px-8 py-6 rounded-3xl border border-stone-400 hover:shadow-2xl">
+          <h4 className="text-3xl pb-1">Some title</h4>
+          {/* Star rating  */}
+          <div className="flex flex-row pb-4 text-xl text-yellow-500">
+            <MdOutlineStarPurple500 />
+            <MdOutlineStarPurple500 />
+            <MdOutlineStarPurple500 />
+            <MdOutlineStarPurple500 />
+            <MdOutlineStarPurple500 />
+          </div>
+          <p className="text-base leading-normal">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem,
+            doloremque expedita neque velit ipsum perspiciatis, temporibus enim,
+            nesciunt cumque corrupti tempora ad dicta animi ullam explicabo nisi
+            dolor doloribus dolore.
+          </p>
+          {/* Date and name of who posted it */}
+          <div className="flex flex-row justify-between items-centers pt-4">
+            {/* customer's name */}
+            <p className="font-bold">John Wayne</p>
+            {/* date when the comment is posted */}
+            <p>24/05/2024</p>
+          </div>
+        </div>
+        {/* Grid Element */}
+        <div className="bg-stone-300 px-8 py-6 rounded-3xl border border-stone-400 hover:shadow-2xl">
+          <h4 className="text-3xl pb-1">Some title</h4>
+          {/* Star rating  */}
+          <div className="flex flex-row pb-4 text-xl text-yellow-500">
+            <MdOutlineStarPurple500 />
+            <MdOutlineStarPurple500 />
+            <MdOutlineStarPurple500 />
+            <MdOutlineStarPurple500 />
+            <MdOutlineStarPurple500 />
+          </div>
+          <p className="text-base leading-normal">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem,
+            doloremque expedita neque velit ipsum perspiciatis, temporibus enim,
+            nesciunt cumque corrupti tempora ad dicta animi ullam explicabo nisi
+            dolor doloribus dolore.
+          </p>
+          {/* Date and name of who posted it */}
+          <div className="flex flex-row justify-between items-centers pt-4">
+            {/* customer's name */}
+            <p className="font-bold">John Wayne</p>
+            {/* date when the comment is posted */}
+            <p>24/05/2024</p>
+          </div>
+        </div>
+        {/* Grid Element */}
+        <div className="bg-stone-300 px-8 py-6 rounded-3xl border border-stone-400 hover:shadow-2xl">
+          <h4 className="text-3xl pb-1">Some title</h4>
+          {/* Star rating  */}
+          <div className="flex flex-row pb-4 text-xl text-yellow-500">
+            <MdOutlineStarPurple500 />
+            <MdOutlineStarPurple500 />
+            <MdOutlineStarPurple500 />
+            <MdOutlineStarPurple500 />
+            <MdOutlineStarPurple500 />
+          </div>
+          <p className="text-base leading-normal">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem,
+            doloremque expedita neque velit ipsum perspiciatis, temporibus enim,
+            nesciunt cumque corrupti tempora ad dicta animi ullam explicabo nisi
+            dolor doloribus dolore.
+          </p>
+          {/* Date and name of who posted it */}
+          <div className="flex flex-row justify-between items-centers pt-4">
+            {/* customer's name */}
+            <p className="font-bold">John Wayne</p>
+            {/* date when the comment is posted */}
+            <p>24/05/2024</p>
+          </div>
+        </div>
+        {/* Grid Element */}
+        <div className="bg-stone-300 px-8 py-6 rounded-3xl border border-stone-400 hover:shadow-2xl">
+          <h4 className="text-3xl pb-1">Some title</h4>
+          {/* Star rating  */}
+          <div className="flex flex-row pb-4 text-xl text-yellow-500">
+            <MdOutlineStarPurple500 />
+            <MdOutlineStarPurple500 />
+            <MdOutlineStarPurple500 />
+            <MdOutlineStarPurple500 />
+            <MdOutlineStarPurple500 />
+          </div>
+          <p className="text-base leading-normal">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem,
+            doloremque expedita neque velit ipsum perspiciatis, temporibus enim,
+            nesciunt cumque corrupti tempora ad dicta animi ullam explicabo nisi
+            dolor doloribus dolore.
+          </p>
+          {/* Date and name of who posted it */}
+          <div className="flex flex-row justify-between items-centers pt-4">
+            {/* customer's name */}
+            <p className="font-bold">John Wayne</p>
+            {/* date when the comment is posted */}
+            <p>24/05/2024</p>
+          </div>
+        </div>
       </div>
     </div>
   );
