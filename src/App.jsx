60c45367493e3567b237c2987174017276a9ced0
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-06-14 23:23:52 +0200

    Routing for about page added!

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import MainLayout from "./layout/MainLayout";
+import AboutPage from "./pages/AboutPage";
 import HomePage from "./pages/HomePage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />)
+      <Route path="/about" element={<AboutPage />} />
     </Route>
   )
 );
