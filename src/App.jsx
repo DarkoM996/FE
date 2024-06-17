34dfe4f78eaa7e30d62a2754ae648c9facfa32b3
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-06-17 14:14:08 +0200

    New pages and routing added

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,16 @@ import {
 import MainLayout from "./layout/MainLayout";
 import AboutPage from "./pages/AboutPage";
 import HomePage from "./pages/HomePage";
+import ContactPage from "./pages/ContactPage";
+import ProductsPage from "./pages/ProductsPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />)
       <Route path="/about" element={<AboutPage />} />
+      <Route path="/products" element={<ProductsPage />} />
+      <Route path="/contact" element={<ContactPage />} />
     </Route>
   )
 );
