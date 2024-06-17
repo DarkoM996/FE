06e182c6240ffc090aecdfdeb7b54c10483e248d
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-06-17 16:06:13 +0200

    Initial work for About Page is set up

---
+++ src/components/AboutHero.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AboutHero = () => {
+  return (
+    <div className="max-w-screen-3xl mx-auto px-6 py-4 relative w-full h-screen">
+      AboutHero
+    </div>
+  );
+};
+
+export default AboutHero;
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import AboutHero from "../components/AboutHero";
 
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    // I'm going to leave this as empty fragments with bunch of components in here completing the page
+    <>
+      <AboutHero />
+    </>
+  );
 };
 
 export default AboutPage;
