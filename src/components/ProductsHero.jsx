304f2ba76dc9ce595f1e8989d4d59ae5265f64b1
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-06-18 16:14:37 +0200

    Hero section for Products page done

--- src/components/ProductsHero.jsx
+++ src/components/ProductsHero.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import productsheroimg from "../images/categorycard.jpg";
 
 const ProductsHero = () => {
-  return <div>ProductsHero</div>;
+  return (
+    <div className="max-w-screen-3xl mx-auto relative w-full h-full px-6 py-4">
+      {/* image background */}
+      <div className="relative h-[600px]">
+        {/* Image Container */}
+        <img
+          src={productsheroimg}
+          alt="/"
+          className="object-cover w-full  h-full rounded-3xl"
+        />
+        {/* Text Container */}
+        <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-end items-start pl-8 py-24 bg-black/40 rounded-3xl text-white">
+          <h1 className="text-5xl md:text-6xl">Products</h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductsHero;
