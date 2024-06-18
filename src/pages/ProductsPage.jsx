bf560f76d67b6c5be37f2e61bafd4e7b082a23bc
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-06-18 16:25:14 +0200

    Initial set up

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Products from "../components/Products";
 import ProductsHero from "../components/ProductsHero";
 
 const ProductsPage = () => {
   return (
     <>
       <ProductsHero />
+      <Products />
     </>
   );
 };
