5f20b3cfcb862df8dc81decfd451f93bdb3a4601
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-06-18 15:59:42 +0200

    Initial section in the Products Hero added

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import ProductsHero from "../components/ProductsHero";
 
 const ProductsPage = () => {
-  return <div>ProductsPage</div>;
+  return (
+    <>
+      <ProductsHero />
+    </>
+  );
 };
 
 export default ProductsPage;
