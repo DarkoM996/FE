db9f9878db005c37677549ef554cca713b983cea
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-06-23 01:29:50 +0200

    Progress on this moving for now

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -6,12 +6,14 @@ import image4 from "../images/categorycard4.jpg";
 import image5 from "../images/categorycard5.jpg";
 import image6 from "../images/categorycard6.jpg";
 import AsideMenu from "./AsideMenu";
+import products from "./../products";
+import TrendingProducts from "./TrendingProducts";
 
 const Products = () => {
   return (
     <div className="max-w-screen-3xl mx-auto relative w-full h-full px-6 py-4">
       {/* Left side filter */}
-      <div className="flex flex-col sm:flex-row justify-between items-start gap-2 w-full">
+      <div className="flex flex-col sm:flex-row justify-between items-start gap-2 w-full px-6">
         <div className="flex flex-row justify-center items-center gap-2">
           <h2 className="text-2xl">Filter By:</h2>
           <button className="p-4 bg-primary_lighterShade-300 hover:bg-primary_lighterShade-500 text-black rounded-full border border-primary_lighterShade-400 w-[160px]">
@@ -25,7 +27,9 @@ const Products = () => {
             Most Popular
           </button>
         </div>
+        {/* Products below */}
       </div>
+      <TrendingProducts />
     </div>
   );
 };
