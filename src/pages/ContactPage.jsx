496d597a86d9174a02a4b0dd32f4c980731fcc7e
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-06-24 13:29:40 +0200

    fixing contact form with significant progress

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -13,11 +13,11 @@ const ContactPage = () => {
             className="object-cover w-full h-full bg-center rounded-3xl"
           />
         </div>
-        <div className="flex flex-col md:flex-row justify-center items-center w-full gap-8 ">
+        <div className="flex flex-col md:flex-row justify-center items-start w-full gap-8 ">
           {/* Heading */}
           <div className="flex flex-col justify-center items-start">
             <h1 className="text-5xl md:text-6xl py-6">Contact Page</h1>
-            <p className="text-balance">
+            <p className="text-balance md:w-2/3">
               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui non
               quis porro exercitationem hic, harum deserunt dolore soluta odit
               autem minima explicabo laborum atque alias nemo quam at fugit
@@ -27,26 +27,26 @@ const ContactPage = () => {
           {/* Contact Form */}
           <form
             action="#"
-            className="max-w-screen-lg p-8 mx-auto w-full h-full flex flex-col justify-center items-center gap-6 md:gap-12 rounded-2xl border border-black/20"
+            className="max-w-screen-md p-8 mx-auto w-full h-full flex flex-col justify-center items-center gap-6 md:gap-12 rounded-2xl border border-black/20"
           >
-            <div className="flex flex-col justify-start items-start gap-2">
+            <div className="flex flex-col justify-between items-center gap-2 w-full h-full">
               <p className="text-transform: uppercase">Contact Page</p>
             </div>
             <div className="flex flex-col gap-4 justify-center items-center w-full h-1/2">
               <input
-                className=" p-4 bg-white hover:bg-[#f1f1f1] rounded-full w-full md:w-2/3"
+                className=" p-4 bg-white hover:bg-[#f1f1f1] rounded-full w-full"
                 type="email"
                 placeholder="E.g Adam Smith"
                 name="Smith"
               />
               <input
-                className=" p-4 bg-white hover:bg-[#f1f1f1] rounded-full w-full md:w-2/3"
+                className=" p-4 bg-white hover:bg-[#f1f1f1] rounded-full w-full"
                 type="email"
                 placeholder="E.g adamsmith93@gmail.com"
                 name="email"
               />
               <textarea
-                className=" p-4 bg-white hover:bg-[#f1f1f1] rounded-2xl w-full md:w-2/3"
+                className=" p-4 bg-white hover:bg-[#f1f1f1] rounded-2xl w-full"
                 name="message"
                 rows="10"
                 placeholder="Your message here"
@@ -60,3 +60,39 @@ const ContactPage = () => {
 };
 
 export default ContactPage;
+
+// Contact Form
+
+{
+  /* Contact Form */
+}
+{
+  /* <form
+action="#"
+className=" max-w-screen-md p-8 mx-auto w-full h-full flex flex-col justify-center items-center gap-6 md:gap-12 rounded-2xl border border-black/20"
+>
+<div className="flex flex-col justify-between items-start gap-2 w-full h-full">
+  <p className="text-transform: uppercase">Contact Page</p>
+</div>
+<div className="flex flex-col gap-4 justify-center items-center w-full h-1/2">
+  <input
+    className=" p-4 bg-white hover:bg-[#f1f1f1] rounded-full w-full md:w-2/3"
+    type="email"
+    placeholder="E.g Adam Smith"
+    name="Smith"
+  />
+  <input
+    className=" p-4 bg-white hover:bg-[#f1f1f1] rounded-full w-full md:w-2/3"
+    type="email"
+    placeholder="E.g adamsmith93@gmail.com"
+    name="email"
+  />
+  <textarea
+    className=" p-4 bg-white hover:bg-[#f1f1f1] rounded-2xl w-full md:w-2/3"
+    name="message"
+    rows="10"
+    placeholder="Your message here"
+  />
+</div>
+</form> */
+}
