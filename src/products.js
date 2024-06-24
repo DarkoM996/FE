2ec4170ec999d0dfacd06af6d40496886a145ee7
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-06-24 15:47:08 +0200

    adding some extra products

--- src/products.js
+++ src/products.js
@@ -148,6 +148,36 @@ const products = [
     rating: 4.0,
     numReviews: 20,
   },
+
+  {
+    id: "11",
+    name: "Featured Product 11",
+    subtitle: "Subtitle #11",
+    image: "/src/images/categorycard.jpg",
+    description:
+      "Fits perfectly in whatever NAMED unit and other units with shelves which are at least 38 cm deep. A good size for everything from games and media accessories to clothes and hobby things. Easy to pull out as the box has a handle. If you organise your things and give them a specific place in the drawer, they will be easier to find.",
+    brand: "Apple",
+    category: "Electronics",
+    price: "$190.00",
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+
+  {
+    id: "12",
+    name: "Featured Product 12",
+    subtitle: "Subtitle #12",
+    image: "/src/images/categorycard3.jpg",
+    description:
+      "Fits perfectly in KALLAX shelving unit and other units with shelves which are at least 38 cm deep. A good size for everything from games and media accessories to clothes and hobby things. Easy to pull out as the box has a handle. If you organise your things and give them a specific place in the drawer, they will be easier to find.",
+    brand: "Apple",
+    category: "Electronics",
+    price: "$80.00",
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
 ];
 
 export default products;
