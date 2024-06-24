c9490fe8f70eb954627f7939c4c0ed3b5473037f
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-06-24 15:42:16 +0200

    Some spacing fixing

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -17,17 +17,17 @@ const ContactPage = () => {
           {/* Heading */}
           <div className="flex flex-col justify-center items-start">
             <h1 className="text-5xl md:text-6xl py-6">Contact Page</h1>
-            <p className="text-balance md:w-2/3">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui non
-              quis porro exercitationem hic, harum deserunt dolore soluta odit
-              autem minima explicabo laborum atque alias nemo quam at fugit
-              perspiciatis.
+            <p className="text-balance w-full lg:w-1/2">
+              Simply fill out the form that we have provided here with the basic
+              info and tell us about what you want to contact us. Your message
+              will be read and someone will get in contact from our support
+              team.
             </p>
           </div>
           {/* Contact Form */}
           <form
             action="#"
-            className="max-w-screen-md py-4 lg:py-8 mx-auto w-full h-full flex flex-col justify-center items-center gap-6 rounded-2xl bg-primary_lighterShade-50"
+            className="max-w-screen-md py-6 lg:py-8 mx-auto w-full h-full flex flex-col justify-center items-center gap-6 rounded-2xl bg-primary_lighterShade-50"
           >
             <div className="flex flex-col justify-between items-start w-full h-full">
               <h2 className="text-2xl md:text-3xl">Contact Page</h2>
