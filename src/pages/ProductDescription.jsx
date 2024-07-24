d8ea962dae8e56b430079f31211d4bdfb9096cd7
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-24 20:20:16 +0200

    latest

--- src/pages/ProductDescription.jsx
+++ src/pages/ProductDescription.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const ProductDescription = () => {
-  return <div>ProductDescription</div>;
+  return (
+    <div className="max-w-screen-3xl mx-auto w-full min-h-screen relative px-6 py-4">
+      ProductDescription
+    </div>
+  );
 };
 
 export default ProductDescription;
