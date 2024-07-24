d5fbdc269ef8a3ed4e6dace4a67d364724900b62
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-24 20:24:41 +0200

    latest

--- src/pages/ProductDescription.jsx
+++ src/pages/ProductDescription.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import products from "../products";
 
 const ProductDescription = () => {
+  const { id: productId } = useParams();
+
+  const product = products.find((p) => p.id === productId);
+  console.log(product);
+
   return (
     <div className="max-w-screen-3xl mx-auto w-full min-h-screen relative px-6 py-4">
       ProductDescription
